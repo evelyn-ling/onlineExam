c121a8582d86815918f980b3cc379e8aa2da384b
evelyn-ling <10832009@st.fhjh.tp.edu.tw>
2020-07-16 10:51:31 +0800

    First commit

---
+++ app/src/main/java/com/evelyn/onlineexam/MainActivity.java
@@ -0,0 +1,24 @@
+package com.evelyn.onlineexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I love coding", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
